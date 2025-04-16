37cc47746de12b0ca28a9a632f07a7dc8520c983
kkalavath <32565778+kkalavath@users.noreply.github.com>
2025-04-17 00:43:28 +0100

    1.2

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import os
+import json
 import app
 
 def test_model_file_created():
@@ -6,6 +7,18 @@ def test_model_file_created():
     assert os.path.exists('models/model.pkl')
 
 def test_model_score():
+    print("Score:")
     score = app.main()  # Assuming the main function returns the score
     assert isinstance(score, float)
     assert 0.0 <= score <= 1.0
+    print(score)
+    # Load the model scores
+    with open('model_scores.json', 'r') as f:
+        model_scores = json.load(f)
+    # Get the latest model score
+    latest_score = model_scores[-1]['score'] 
+    print("Latest_Score:", latest_score)
+    # Compare the latest score with the current score
+    assert score >= latest_score
+
+    
