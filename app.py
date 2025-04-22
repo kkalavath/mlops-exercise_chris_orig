ba012d7454ff38c41a97862f5ee17c8e7e47fc81
kkalavath <32565778+kkalavath@users.noreply.github.com>
2025-04-22 22:30:31 +0100

    1.5

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def split_data(data, target_column, test_size=0.2, random_state=42):
 
 def train_model(X_train, y_train):
     model = LogisticRegression()
+    model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
     return model
